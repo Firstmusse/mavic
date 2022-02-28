7658181e7cf7c8f8941265f424e4b75150a6de03
Alexander Stepanenko <mussefirst1@gmail.com>
2022-02-28 22:51:28 +0600

    jq анимация

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
 
+
+$(window).scroll(function() {
+    let height = $(window).scrollTop();
+    if(height > 20){
+    $('header').addClass('header-fixed');
+    } else{
+    $('header').removeClass('header-fixed');
+    }
 });
