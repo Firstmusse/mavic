5d87ced1fad5ab31ab10941aea62e5f1780d93b4
Aleksandr <58659614+Firstmusse@users.noreply.github.com>
2022-05-24 16:38:40 +0600

    fix mobile-menu btn

--- js/main.js
+++ js/main.js
@@ -1,47 +1,39 @@
-
-$(window).scroll(function() {
-    let height = $(window).scrollTop();
-    if(height > 20){
-    $('header').addClass('header-fixed');
-    } else{
-    $('header').removeClass('header-fixed');
-    }
+$(window).scroll(function () {
+  let height = $(window).scrollTop();
+  if (height > 20) {
+    $("header").addClass("header-fixed");
+  } else {
+    $("header").removeClass("header-fixed");
+  }
 });
 
 $(document).ready(function () {
-    $('.slider').slick({
-        infinity: true,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
-        // fade:true,
-        autoplay: true,
-        autoplaySpeed: 1000,
+  $(".slider").slick({
+    infinity: true,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
 
-    });
-
-          
-      $(".questions__item-title-arrow").click(function() {
-          console.dir(this)
-        $(this).find("~ .questions__item-text").toggle(200);
-        $(this).children(".arrow-up").toggleClass('rotate');
-
-
-      });
-       
-
-      
-
+  $(".questions__item-title-arrow").click(function () {
+    console.dir(this);
+    $(this).find("~ .questions__item-text").toggle(200);
+    $(this).children(".arrow-up").toggleClass("rotate");
+  });
 });
- // mobile.style.backgroundImage = "url('images/closed.svg')"
 
-
-const mobile = document.getElementById('mobile-click')
-const menu = document.getElementById('menu-m')
-
-const mobileActive = 
-
-mobile.onclick = function(){
-  mobile.classList.toggle('active');
-  menu.style.display = "flex";
-
-}
+const mobile = document.getElementById("mobile-click");
+const menu = document.getElementById("menu-m");
+let mobileActive = false;
+mobile.onclick = function () {
+  if (!mobileActive) {
+    mobile.classList.toggle("active");
+    menu.style.display = "flex";
+    mobileActive = true;
+  } else {
+    mobile.classList.toggle("active");
+    menu.style.display = "none";
+    mobileActive = false;
+  }
+};
